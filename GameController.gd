a03af8212ddb947bef607b38a446a71a7fd72948
Thylk <regnault.maxime@gmail.com>
2024-05-21 19:07:26 +0200

    feat: added GameController logic

---
+++ GameController.gd
@@ -0,0 +1,39 @@
+extends Node3D
+
+var dice_values = {}  # Holds die instance ID as key and rolled value as value
+var target_values = [4, 2, 1]  # Desired values
+var expected_dice_count = 3  # Total number of dice you expect to roll
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var dice_nodes = get_tree().get_nodes_in_group("dice") # Assuming you've added all dice to the 'dice' group
+	for die_node in dice_nodes:
+		die_node.connect("roll_finished", Callable(self, "_on_roll_finished"))
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func check_dice_values():
+	# Convert the dictionary values to a sorted list
+	var rolled_values = dice_values.values()
+	rolled_values.sort()
+	
+	# Check if the rolled values are the same as the target values
+	if rolled_values == target_values:
+		print("The dice rolled 4, 2, and 1!")
+	else:
+		print("The dice did not roll the target values.")
+
+	# Clear the previous dice values for future rolls
+	dice_values.clear()
+
+func _on_roll_finished(die_id, value):
+	print("Die stopped rolling with the upward-facing value of: ", value)
+	
+	# Track the value reported by the die, using its unique instance ID
+	dice_values[die_id] = value
+
+	# Check if all dice have reported their values
+	if len(dice_values) == expected_dice_count:
+		check_dice_values()
--- die.gd
+++ die.gd
@@ -2,12 +2,16 @@ extends RigidBody3D
 
 var start_pos
 var roll_strength = 30
+var markers = []
 
 signal roll_finished(value)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	start_pos = global_position
+	# Add your marker nodes to the array
+	for i in range(1,7):
+		markers.append(get_node(str(i)))
 
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
@@ -29,6 +33,27 @@ func _roll():
 	var throw_vector = Vector3(randf_range(-1, 1), 0, randf_range(-1, 1)).normalized()
 	angular_velocity = throw_vector * roll_strength / 2
 	apply_central_impulse(throw_vector * roll_strength)
+	
+func get_upward_facing_value():
+	var highest_marker_index = -1
+	var highest_y = -INF  # Start with negative infinity
+
+	for i in range(markers.size()):
+		var marker_global_Y = markers[i].global_transform.origin.y
+		if marker_global_Y > highest_y:
+			highest_y = marker_global_Y
+			highest_marker_index = i
+
+	# Assuming marker names correlate with face values directly
+	return highest_marker_index + 1
+	
+func _integrate_forces(state):
+	# Check if the die has come to rest
+	if linear_velocity.length() < 0.01 and angular_velocity.length() < 0.01:
+		# Die is considered resting, get the upward facing value
+		var upward_value = get_upward_facing_value()
+		emit_signal("roll_finished", get_instance_id(), upward_value)
+		sleeping = true  # Put the die to sleep to stop processing forces
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
